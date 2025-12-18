feea91aa97c9d5c0e59e85601b0c9063ebeb2c73
Volodymyr Yelisieiev <v.yelisieiev@gmail.com>
2025-12-18 01:21:36 +0100

    feat: add initial database migration

---
+++ migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20251218_002029 from './20251218_002029';
+
+export const migrations = [
+  {
+    up: migration_20251218_002029.up,
+    down: migration_20251218_002029.down,
+    name: '20251218_002029'
+  },
+];
